4a6a6d1de892917b62d57eaefd3501378b9db73a
meHarshh <meharshhk@gmail.com>
2024-07-29 19:53:14 +0530

    changed ip address

--- server/index.js
+++ server/index.js
@@ -26,13 +26,13 @@ app.get('/', async (req, res) => {
 const server = app.listen(port, () => {
     const protocol = (process.env.HTTPS === true || process.env.NODE_ENV === 'production') ? 'https' : 'http';
     const { address, port } = server.address();
-    const host = address === '::' ? '127.0.1.1' : address;
+    const host = address === '::' ? '127.0.0.1' : address;
     console.log(`Server listening at ${protocol}://${host}:${port}/`);
 });
 
 
 // Connect to MongoDB
-const DATABASE_URL = process.env.DB_URL || 'mongodb://127.0.1.1:27017'
+const DATABASE_URL = process.env.DB_URL || 'mongodb://127.0.0.1:27017'
 const DATABASE = process.env.DB || 'Prolink'
 
 db(DATABASE_URL, DATABASE);
